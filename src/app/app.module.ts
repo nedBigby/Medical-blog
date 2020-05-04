1731abf3d5cf1119cf7b624e71c418cc66dbb669
corporatesnub <ddimela9no@gmail.com>
2020-05-04 10:55:16 -0400

    Lean files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { PresentationComponent} from './presentation/presentation.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,23 +16,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [AppComponent,
-    HeaderComponent,
-    SidenavComponent,
-    PresentationComponent,
-    PresentationComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatChipsModule,
-    MatGridListModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
