ecb8ea7c14de4e342f8ee97fff7d4b5d80add5ad
corporatesnub <ddimela9no@gmail.com>
2020-05-12 16:27:19 -0400

    fixed all sizes

--- src/app/landing/landing-esp/landing-esp.component.ts
+++ src/app/landing/landing-esp/landing-esp.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingEspComponent implements OnInit {
 
+
+  private cards = [
+    {
+      title: "Estética",
+      img: "../../../assets/c700x420.jpg",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gall"
+    },
+    {
+      title: "Anti-aging",
+      img: "../../../assets/a4ac0fe02f324aa1597d405ac475837a.jpg",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gall"
+    },
+    {
+      title: "Nutrición",
+      img: "../../../assets/hilos-de-la-dna-simples-y-hélice-dual-16944508.jpg",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gall"
+    },
+    {
+      title: "Genética",
+      img: "../../../assets/magnesio-770x550.png",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gall"
+    }
+  ]
+
   constructor() { }
+
+  getCards = () =>{
+    return this.cards;
+  }
 
   ngOnInit(): void {
   }
