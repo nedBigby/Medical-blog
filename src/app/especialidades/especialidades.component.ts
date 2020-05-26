3f4358980b7c3a6f40e5638352371388f1600d1c
corporatesnub <ddimela9no@gmail.com>
2020-05-26 02:49:09 -0400

    animations + routing

--- src/app/especialidades/especialidades.component.ts
+++ src/app/especialidades/especialidades.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EspecialidadesComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,9 @@ import { Router } from '@angular/router';
 
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  @Input() pageSelected:string;
+
+  constructor(private router: Router) {}
 
   goToLanding = () =>{this.router.navigate(['/landing'])};
   goToBlog = () =>{this.router.navigate(['/blog'])};
